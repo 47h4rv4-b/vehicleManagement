bfb0fc5c39f30e2a85c23721348c99917bf41da4
atharvaBaste <msm19b024@iiitdm.ac.in>
2023-07-11 00:39:36 +0530

    getDetails

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from typing import List
 from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, Field
+from fastapi import HTTPException
+from fastapi.responses import JSONResponse
 # from controllers.authentication.login import auth0_login
 # from controllers.authentication.logout import auth0_logout
 # from controllers.routes.add_vehicle import add_vehicle
@@ -89,19 +92,67 @@ def root(request: Request):
 #     return fetch_vehicle(get_vehicle_model)
 
 
-# Model for bike details
-# class BikeDetails(BaseModel):
-#     registration_number: str = Field(..., description="Registration number")
-#     bike_brand: str = Field(..., description="Brand name")
-#     bike_model: str = Field(..., description="Model name")
-#     ownership_status: str = Field(..., description="Bike owner")
-#     year: int = Field(..., description="Year of manufacture")
-#     location: str = Field(..., description="Location")
-#     kms_driven: int = Field(..., description="No. of kilometers driven")
-#     power: int = Field(..., description="Bike power")
-#     timestamp: int = Field(..., description="Current timestamp")
+class BikeDetails(BaseModel):
+    registration_number: str = Field(..., description="Registration number")
+    bike_brand: str = Field(..., description="Brand name")
+    bike_model: str = Field(..., description="Model name")
+    ownership_status: str = Field(..., description="Bike owner")
+    year: int = Field(..., description="Year of manufacture")
+    location: str = Field(..., description="Location")
+    kms_driven: int = Field(..., description="No. of kilometers driven")
+    power: int = Field(..., description="Bike power")
+    timestamp: int = Field(..., description="Current timestamp")
 
-    # Add other fields as needed
+def get_dummy_data():
+    dummy_data = [
+    {
+        "registration_number": "ABCD1234",
+        "bike_brand": "Example Brand 1",
+        "bike_model": "Example Model 1",
+        "ownership_status": "Example Owner 1",
+        "year": 2022,
+        "location": "Example Location 1",
+        "kms_driven": 5000,
+        "power": 150,
+        "timestamp": 1621375879,
+    },
+    {
+        "registration_number": "EFGH5678",
+        "bike_brand": "Example Brand 2",
+        "bike_model": "Example Model 2",
+        "ownership_status": "Example Owner 2",
+        "year": 2021,
+        "location": "Example Location 2",
+        "kms_driven": 8000,
+        "power": 200,
+        "timestamp": 1621375890,
+    },
+    {
+        "registration_number": "WXYZ9876",
+        "bike_brand": "Example Brand 3",
+        "bike_model": "Example Model 3",
+        "ownership_status": "Example Owner 3",
+        "year": 2020,
+        "location": "Example Location 3",
+        "kms_driven": 10000,
+        "power": 180,
+        "timestamp": 1621375901,
+    },
+    {
+        "registration_number": "PQRS5432",
+        "bike_brand": "Example Brand 4",
+        "bike_model": "Example Model 4",
+        "ownership_status": "Example Owner 4",
+        "year": 2019,
+        "location": "Example Location 4",
+        "kms_driven": 12000,
+        "power": 250,
+        "timestamp": 1621375912,
+    }
+]
+
+    return dummy_data
+
 
 
 @app.get("/", tags=["Default route"])
@@ -109,32 +160,24 @@ def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-# @app.post("/get_bike_details", tags=["Vehicle details"])
-# def get_bike_details(bike_info: BikeDetails):
-#     # Perform database query or any other logic to get bike details based on registration number
-#     registration_number = bike_info.registration_number
-#     # Assuming you have a function to get bike details, update the logic below accordingly
-#     bike_details = get_bike_details_from_database(registration_number)
-#     return bike_details
+@app.post("/get_bike_details", tags=["Vehicle details"])
+def get_bike_details(bike_info: GetVehicle):
+    registration_number = bike_info.registration_number
+    bike_details = get_bike_details_from_database(registration_number)
+    if bike_details is None:
+        raise HTTPException(status_code=404, detail="Bike details not found")
+    return JSONResponse(content=bike_details)
 
 
 def get_bike_details_from_database(registration_number: str):
-    # Assuming you have a database connection and query to fetch bike details
-    # Replace the code below with your actual database query logic
-    bike_details = {
-        "registration_number": registration_number,
-        "bike_brand": "Example Brand",
-        "bike_model": "Example Model",
-        "ownership_status": "Example Owner",
-        "year": 2022,
-        "location": "Example Location",
-        "kms_driven": 5000,
-        "power": 150,
-        "timestamp": 1621375879,
-    }
-    return bike_details
+    dummy_data=get_dummy_data()
+    for bike_details in dummy_data:
+        if bike_details["registration_number"] == registration_number:
+            # Convert datetime object to string representation
+            bike_details["timestamp"] = str(bike_details["timestamp"])
+            return bike_details
+    return None
 
-# Add more routes or functions as needed
 
 
 # Run the app
